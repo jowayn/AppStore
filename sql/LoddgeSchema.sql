5b9a22d99d35d28301c23e920473a8911afd6a87
tan-yx <101549235+tan-yx@users.noreply.github.com>
2022-03-21 18:13:09 +0800

    Update LoddgeSchema.sql

--- sql/LoddgeSchema.sql
+++ sql/LoddgeSchema.sql
@@ -3,11 +3,11 @@ CREATE TABLE IF NOT EXISTS user_base(
 	user_password VARCHAR(64) NOT NULL, 
 	first_name VARCHAR(64) NOT NULL, 
 	last_name VARCHAR(64) NOT NULL, 
-	phone_number VARCHAR(15) NOT NULL
+	phone_number NUMERIC NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS listings(
-	listing_id VARCHAR(64) PRIMARY KEY, 
+	listing_id INT PRIMARY KEY, 
 	listing_name VARCHAR(64) NOT NULL,
 	neighbourhood VARCHAR(64) NOT NULL,
 	neighbourhood_group VARCHAR(64) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS listings(
 	longitude NUMERIC NOT NULL CHECK(longitude >= -180 AND longitude <= 180),
 	room_type VARCHAR(64) NOT NULL,
 	price MONEY NOT NULL,
-	publish_date date NOT NULL,
+/*	publish_date date NOT NULL, */
 	owner_id VARCHAR(64) REFERENCES user_base(user_id)
 		ON UPDATE CASCADE ON DELETE CASCADE,
 	total_occupancy INT NOT NULL,
@@ -27,22 +27,19 @@ CREATE TABLE IF NOT EXISTS listings(
 	has_heater BOOLEAN NOT NULL
 );
 
-CREATE EXTENSION btree_gist;
+CREATE EXTENSION IF NOT EXISTS btree_gist;
 CREATE TABLE IF NOT EXISTS reservations(
-	reservation_id VARCHAR(20) PRIMARY KEY,
+	reservation_id INT PRIMARY KEY,
 	user_id VARCHAR(64) REFERENCES user_base(user_id)
 		ON UPDATE CASCADE ON DELETE CASCADE,
-	total_price MONEY NOT NULL,
-	listing_id VARCHAR(64) REFERENCES listings(listing_id)
+	listing_id INT REFERENCES listings(listing_id)
 		ON UPDATE CASCADE ON DELETE CASCADE,
 	date_range DATERANGE NOT NULL,
 	EXCLUDE USING GIST (listing_id WITH =, date_range WITH &&)
 );
 
 CREATE TABLE IF NOT EXISTS reviews(
-	reservation_id VARCHAR(20) UNIQUE REFERENCES reservations(reservation_id)
+	reservation_id INT UNIQUE REFERENCES reservations(reservation_id)
 		ON UPDATE CASCADE ON DELETE CASCADE,
-	review DECIMAL(3) NOT NULL CHECK(review >= 1 AND review <= 5)
+	review INT NOT NULL CHECK(review >= 1 AND review <= 5)
 );
-
-
