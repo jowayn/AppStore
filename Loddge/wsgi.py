eab2e5a57945e12416673b8238c1b5c1f7a50997
Jo-Wayn <86486828+jowayn@users.noreply.github.com>
2022-03-22 17:04:12 +0800

    Update wsgi.py

--- Loddge/wsgi.py
+++ Loddge/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AppStore.settings')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Loddge.settings')
 
 application = get_wsgi_application()
