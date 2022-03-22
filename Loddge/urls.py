3467058fcfc3662069e72afa811e954625392637
Jo-Wayn <86486828+jowayn@users.noreply.github.com>
2022-03-22 17:04:40 +0800

    Update urls.py

--- Loddge/urls.py
+++ Loddge/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('add', app.views.add, name='add'),
     path('view/<str:id>', app.views.view, name='view'),
     path('edit/<str:id>', app.views.edit, name='edit'),
-]
+] 
