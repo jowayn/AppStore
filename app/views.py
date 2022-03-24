aacd2921fb8175be5cd66cd69dd930d5910a040a
Jo-Wayn <86486828+jowayn@users.noreply.github.com>
2022-03-25 03:14:40 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -137,11 +137,12 @@ def marketplace(request):
 
         with connection.cursor() as cursor:
             cursor.execute(
-                "
+                """
                 SELECT * 
                 FROM listings l
                 ORDER BY l.listing_id
-                "),
+                """
+                ),
             listings = cursor.fetchall()
 
         result_dict = {'records': listings}
