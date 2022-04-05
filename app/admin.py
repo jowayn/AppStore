7f449792fe995ec3e2014352f54edc54dbe6a958
Jo-Wayn <86486828+jowayn@users.noreply.github.com>
2022-04-05 14:14:45 +0800

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+"""
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import gettext_lazy as _
 
@@ -6,8 +8,10 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
-    """Define admin model for custom User model with no email field."""
 
+"""
+    """Define admin model for custom User model with no email field."""
+"""
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name')}),
@@ -24,3 +28,4 @@ class UserAdmin(DjangoUserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
+"""
