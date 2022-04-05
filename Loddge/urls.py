0ab58e40afc47b8a3a010ddbd059d4bf8b3bb91b
Jo-Wayn <86486828+jowayn@users.noreply.github.com>
2022-04-05 17:34:41 +0800

    Update urls.py

--- Loddge/urls.py
+++ Loddge/urls.py
@@ -22,6 +22,7 @@ import app.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', app.views.index, name='index'),
+    path('home/', app.views.home, name='home'),
     path('register/', app.views.register, name='register'),
     path('marketplace/', app.views.marketplace, name='marketplace'),
     path('marketplace/add/', app.views.add, name='add'),
