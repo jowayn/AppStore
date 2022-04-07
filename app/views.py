cf912453c717fcb96b169d8f747e40d88a1402f3
Jo-Wayn <86486828+jowayn@users.noreply.github.com>
2022-04-08 00:41:47 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -300,15 +300,14 @@ def marketplace(request):
             with connection.cursor() as cursor:
                 cursor.execute(
                 """
-                SELECT l.listing_id,
+                SELECT * FROM
+                (SELECT l.listing_id,
                     l.listing_name,
                     l.neighbourhood, l.neighbourhood_group, l.address, l.room_type,
                     l.price, CASE WHEN a.average_review is NULL THEN 0 ELSE a.average_review END,
                     l.owner_id, l.total_occupancy, l.total_bedrooms
                 FROM
                     listings l
-                WHERE neighbourhood_group = %s
-                    AND total_occupancy >= %s
                 LEFT JOIN
                 (SELECT res.listing_id,
                 AVG(rev.review)::NUMERIC(3,2) AS average_review
@@ -316,7 +315,9 @@ def marketplace(request):
                 WHERE rev.reservation_id = res.reservation_id
                 GROUP BY res.listing_id) AS a
                 ON l.listing_id = a.listing_id
-                ORDER BY l.listing_id
+                ORDER BY l.listing_id)
+                WHERE neighbourhood_group = %s
+                    AND total_occupancy >= %s
                 """,
                 [
                     request.POST['neighbourhood_group'],
